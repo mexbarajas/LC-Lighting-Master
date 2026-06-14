d050b165cfd38c36551071e1b7c6a74ec6dd9389
Mario Romero Barajas <mario.romero@cooperlighting.com>
2026-06-14 13:42:42 -0400

    fix: add shortcut icon link for Firefox favicon support

--- app/layout.js
+++ app/layout.js
@@ -267,6 +267,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" type="image/png" sizes="32x32" href="/brand/favicon-32.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
